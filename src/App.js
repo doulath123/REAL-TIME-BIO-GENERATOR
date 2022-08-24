cb2986e2d9b63f74aed2c650697321c0055a018a
NAYAB SYED DOULATH BASHA <96289292+doulath123@users.noreply.github.com>
2022-08-24 16:17:13 +0530

    create-react-app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      REAL-TIME-BIO-GENERATOR-APP
+    </div>
+  );
+}
+
+export default App;
